ad8f67a561010cf93db9b8332984b1689c542be6
arqies <contact@arqies.com>
2025-11-01 12:07:19 +0530

    restore old update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -88,19 +88,14 @@ export default function Home() {
 
 
           <div className='imgname '>
-             <Image alt='alternate text' src={logo} className='logo' style={{border-radius:'100%'}}></Image>
+             <Image alt='alternate text' src={logo} className='logo'></Image>
             <h1 className='name'>Arqies</h1>
           </div>
           <div className='flex flex-col abmedia justify-center items-center'>
-            <p className='aboutme'>Hi, I am Arqies. A self-taught (and still learning) Software/AI Engineer from India.</p>
+            <p className='aboutme'>Hi, I am Arqies. A self-taught Software Engineer who's now learning about AI and ML.</p>
 
             <div className='flex gap-0'>
-                  <li className="flex flex-warp gap-5 medialist">
-                      <a href="https://discordapp.com/users/772047297961525269" target="_blank" ><Image alt='ase' className="testcolor"src={dc} ></Image></a>
-                      <a href="https://github.com/arqies" target="_blank" ><Image alt='ase' className="testcolor"src={git} ></Image></a>
-                      <a href="mailto: contact@arqies.com" target="_blank" ><Image alt='ase' className="testcolor"src={mail} ></Image></a>
-                    
-                  </li>
+      
 
             </div>
             </div>
